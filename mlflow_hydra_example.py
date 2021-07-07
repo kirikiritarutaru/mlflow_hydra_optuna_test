9a9e842783c04480dd9ea2046301fb180a29f043
taru <kirikia@gmail.com>
2021-07-07 23:05:56 +0900

    update

---
+++ mlflow_hydra_example.py
@@ -0,0 +1,96 @@
+import os
+import sys
+
+import hydra
+import mlflow
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from hydra import utils
+from model import SAMPLE_DNN
+from omegaconf import DictConfig, ListConfig
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+from torchvision.datasets import MNIST
+
+
+def log_params_from_omegaconf_dict(params):
+    for param_name, element in params.items():
+        _explore_recursive(param_name, element)
+
+
+def _explore_recursive(parent_name, element):
+    if isinstance(element, DictConfig):
+        for k, v in element.items():
+            if isinstance(v, DictConfig) or isinstance(v, ListConfig):
+                _explore_recursive(f'{parent_name}.{k}', v)
+            else:
+                mlflow.log_param(f'{parent_name}.{k}', v)
+    elif isinstance(element, ListConfig):
+        for i, v in enumerate(element):
+            mlflow.log_param(f'{parent_name}.{i}', v)
+
+
+@hydra.main(config_path='conf', config_name='config_example')
+def main(cfg):
+    dataset = MNIST(
+        '~/src/mlflow_hydra_optuna_test',
+        download=True,
+        transform=transforms.ToTensor()
+    )
+    train, val = random_split(dataset, [55000, 5000])
+
+    trainloader = DataLoader(
+        train, batch_size=cfg.train.batch_size, shuffle=True
+    )
+    testloader = DataLoader(val, batch_size=cfg.test.batch_size, shuffle=False)
+
+    # load model
+    model = SAMPLE_DNN(cfg)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(
+        model.parameters(),
+        lr=cfg.optimizer.lr,
+        momentum=cfg.optimizer.momentum
+    )
+
+    # start new run
+    mlflow.set_tracking_uri('file://' + utils.get_original_cwd() + '/mlruns')
+    mlflow.set_experiment(cfg.mlflow.runname)
+    with mlflow.start_run():
+        for epoch in range(cfg.train.epoch):
+            running_loss = 0.0
+
+            # log param
+            log_params_from_omegaconf_dict(cfg)
+            for i, (x, y) in enumerate(trainloader):
+                steps = epoch * len(trainloader) + i
+                optimizer.zero_grad()
+
+                outputs = model(x)
+                loss = criterion(outputs, y)
+                loss.backward()
+                optimizer.step()
+
+                running_loss += loss.item()
+
+                # log metric
+                mlflow.log_metric("loss", loss.item(), step=steps)
+
+            correct = 0
+            total = 0
+            with torch.no_grad():
+                for (x, y) in testloader:
+                    outputs = model(x)
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += y.size(0)
+                    correct += (predicted == y).sum().item()
+
+            accuracy = float(correct / total)
+            mlflow.log_metric('acc', accuracy, step=epoch)
+
+    return accuracy
+
+
+if __name__ == '__main__':
+    main()
---
+++ model.py
@@ -0,0 +1,18 @@
+import os
+
+import torch.nn as nn
+
+
+class SAMPLE_DNN(nn.Module):
+    def __init__(self, cfg):
+        super(SAMPLE_DNN, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(28 * 28, cfg.model.node1),
+            nn.ReLU(),
+            nn.Linear(cfg.model.node1, cfg.model.node2),
+            nn.ReLU(),
+            nn.Linear(cfg.model.node2, 10)
+        )
+
+    def forward(self, x):
+        return self.model(x.view(x.size(0), -1))
